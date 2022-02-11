b06d015cb7fa54bcb154775c0ec8aecbd0fcdb67
janvanderdoe <j.vdrdoe@tilburguniversity.edu>
2022-02-11 17:24:59 +0100

    loading data and inspection

--- Team_9_Project.R
+++ Team_9_Project.R
@@ -1,3 +1,21 @@
 # Repository for Team 9 Data Preparation and Workflow Management
-test
-test_1
+
+library(data.table)
+library(R.utils)
+listings <- fread("listings.csv.gz")
+calendar <- fread("calendar.csv.gz")
+reviews <- fread("reviews.csv.gz")
+library(tidyverse)
+
+data <- reviews %>% left_join(listings, by = c("listing_id" = "id"), suffix = c("rev", "list")) %>%
+  select(-ends_with("url"))
+
+glimpse(data)
+
+# review scores by neighbourhood, room type and property type
+data$neighbourhood <- as.factor(data$neighbourhood)
+data %>% group_by(neighbourhood) %>% summarize(mean(review_scores_rating))
+data$room_type <- as.factor(data$room_type)
+data %>% group_by(room_type) %>% summarize(mean(review_scores_rating))
+data$property_type <- as.factor(data$property_type)
+data %>% group_by(property_type) %>% summarize(mean(review_scores_rating))
