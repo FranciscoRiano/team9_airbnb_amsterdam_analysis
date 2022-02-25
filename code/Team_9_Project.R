f6660f6a5198954b7a1409210a1268ee58c3f99c
janvanderdoe <j.vdrdoe@tilburguniversity.edu>
2022-02-25 14:35:30 +0100

    Changed the paths

--- code/Team_9_Project.R
+++ code/Team_9_Project.R
@@ -3,15 +3,12 @@
 # Clear workspace
 rm(list =ls())
 
-# Make current directory the working directory. It is important to get the three files and with the setwd function we have to add the path where the 3 files are hosted
-setwd()  
-
 #Import datasets
 library(data.table)
 library(tidyverse)
 
 #Get data
-data <- fread("data/calendar_holiday_distance.csv.gz")
+data <- fread("../data/calendar_holiday_distance.csv.gz")
 View(data)
 
 #Data inspection
