cfed698c2d6fc9a5d3c2afd1c77289117fcab226
janvanderdoe <j.vdrdoe@tilburguniversity.edu>
2022-03-24 13:10:36 +0100

    Added some graphs you can use for the report

--- src/analysis/dprep_analysis.R
+++ src/analysis/dprep_analysis.R
@@ -3,139 +3,88 @@ library(data.table)
 #Get clean and merged dataset
 data <- fread("../../gen/output/calendar_holiday_distance_clean.csv.gz")
 
-#Correlation matrix
-library(Hmisc)
-data %>%
-  select(price, adjusted_price, cs_dist, Holiday) %>%
-  as.matrix() %>%
-  rcorr(type="spearman")
 
-#Scatter plot on subset
-pdf("../../gen/output/scatter_price_distance.pdf")
+#Plot distance variables all in one plot
+library(reshape2)
 library(ggplot2)
-data %>%
-  slice_sample(n=1000000) %>%
-  ggplot(aes(schiphol_dist, price)) +
-  geom_smooth()
+long_data <- melt(data %>% select(price, ends_with("_dist")), id="price")
 
-data %>%
-  slice_sample(n=1000000) %>%
-  ggplot(aes(tram_dist, price)) +
-  geom_smooth()
+pdf("../../gen/output/scatter_price_distance.pdf")
 
-data %>%
-  slice_sample(n=1000000) %>%
-  ggplot(aes(metro_dist, price)) +
-  geom_smooth()
+#Distance on price
+long_data %>%
+  slice_sample(n=1000000) %>% 
+  ggplot(aes(value, price, colour = variable)) +
+  geom_smooth(se=F)
 
-data %>%
-  slice_sample(n=1000000) %>%
-  ggplot(aes(cs_dist, price)) +
-  geom_smooth()
-
-data %>%
-  slice_sample(n=1000000) %>%
-  ggplot(aes(swim_dist, price)) +
-  geom_smooth()
-
-data %>%
-  slice_sample(n=1000000) %>%
-  ggplot(aes(religion_dist, price)) +
-  geom_smooth()
-
-data %>%
-  slice_sample(n=1000000) %>%
-  ggplot(aes(park_dist, price)) +
-  geom_smooth()
-
-data %>%
-  slice_sample(n=1000000) %>%
-  ggplot(aes(sport_dist, price)) +
-  geom_smooth()
-
-# Log transformed distances
-
-data %>%
-  slice_sample(n=1000000) %>%
-  ggplot(aes(log(schiphol_dist), price)) +
-  geom_smooth()
-
-data %>%
-  slice_sample(n=1000000) %>%
-  ggplot(aes(log(tram_dist), price)) +
-  geom_smooth()
-
-data %>%
-  slice_sample(n=1000000) %>%
-  ggplot(aes(log(metro_dist), price)) +
-  geom_smooth()
-
-data %>%
-  slice_sample(n=1000000) %>%
-  ggplot(aes(log(cs_dist), price)) +
-  geom_smooth()
-
-data %>%
-  slice_sample(n=1000000) %>%
-  ggplot(aes(log(swim_dist), price)) +
-  geom_smooth()
-
-data %>%
-  slice_sample(n=1000000) %>%
-  ggplot(aes(log(religion_dist), price)) +
-  geom_smooth()
-
-data %>%
-  slice_sample(n=1000000) %>%
-  ggplot(aes(log(park_dist), price)) +
-  geom_smooth()
-
-data %>%
-  slice_sample(n=1000000) %>%
-  ggplot(aes(log(sport_dist), price)) +
-  geom_smooth()
+#Log distance on price
+long_data %>%
+  slice_sample(n=1000000) %>% 
+  ggplot(aes(log(value), price, colour = variable)) +
+  geom_smooth(se=F)
 
 #Plots of the number of infras in a 500 meter radius
-
+s_size <- 10000
 data %>%
-  slice_sample(n=100000) %>%
+  slice_sample(n=s_size) %>% 
+  drop_na() %>%
+  group_by(metro_number_in_500) %>%
+  summarize(price = mean(price), count = n()) %>% 
   ggplot(aes(metro_number_in_500, price)) +
-  geom_col()
+  geom_col() +
+  geom_text(aes(y = price + 10, label=paste("n=", count)), vjust=0)
 
 data %>%
-  slice_sample(n=100000) %>%
+  slice_sample(n=s_size) %>% 
+  drop_na() %>%
+  group_by(tram_number_in_500) %>%
+  summarize(price = mean(price), count = n()) %>% 
   ggplot(aes(tram_number_in_500, price)) +
-  geom_col()
+  geom_col() +
+  geom_text(aes(y = price + 10, label=paste("n=", count)), vjust=0)
 
 data %>%
-  slice_sample(n=100000) %>%
-  ggplot(aes(swim_number_in_500, price)) +
-  geom_col()
+  slice_sample(n=s_size) %>% 
+  drop_na() %>%
+  group_by(cs_number_in_500) %>%
+  summarize(price = mean(price), count = n()) %>% 
+  ggplot(aes(cs_in_500, price)) +
+  geom_col() +
+  geom_text(aes(y = price + 10, label=paste("n=", count)), vjust=0)
 
 data %>%
-  slice_sample(n=100000) %>%
+  slice_sample(n=s_size) %>% 
+  drop_na() %>%
+  group_by(religion_number_in_500) %>%
+  summarize(price = mean(price), count = n()) %>% 
   ggplot(aes(religion_number_in_500, price)) +
-  geom_col()
+  geom_col() +
+  geom_text(aes(y = price + 10, label=paste("n=", count)), vjust=0)
 
 data %>%
-  slice_sample(n=100000) %>%
+  slice_sample(n=s_size) %>% 
+  drop_na() %>%
+  group_by(park_number_in_500) %>%
+  summarize(price = mean(price), count = n()) %>% 
   ggplot(aes(park_number_in_500, price)) +
-  geom_col()
+  geom_col() +
+  geom_text(aes(y = price + 10, label=paste("n=", count)), vjust=0)
 
 data %>%
-  slice_sample(n=100000) %>%
+  slice_sample(n=s_size) %>% 
+  drop_na() %>%
+  group_by(sport_number_in_500) %>%
+  summarize(price = mean(price), count = n()) %>% 
   ggplot(aes(sport_number_in_500, price)) +
-  geom_col()
+  geom_col() +
+  geom_text(aes(y = price + 10, label=paste("n=", count)), vjust=0, size=2)
 
 dev.off()
 
-<<<<<<< HEAD
 #Regression
-=======
 data$neighbourhood_cleansed <- as.factor(data$neighbourhood_cleansed)
 
 #Regression model
->>>>>>> b5e158134855fef4d74cc340dfb963f4c79496e3
 model_all_int_hol_wkend <- lm(price ~ .- date -listing_id -price - adjusted_price -longitude - latitude -Day -schiphol_number_in_500 + Weekend*metro_dist + Weekend*tram_dist + Weekend*schiphol_dist + Weekend*cs_dist + Holiday*metro_dist + Holiday*tram_dist + Holiday*cs_dist + Holiday*schiphol_dist, data,  na.action = na.omit)
 summary(model_all_int_hol_wkend)
 
@@ -148,8 +97,13 @@ model_poly_log_int <- lm(price ~ log(tram_dist) + tram_number_in_500 + I(log(tra
                            log(schiphol_dist)+ I(log(schiphol_dist)^2) +
                            log(sport_dist) + sport_number_in_500 + I(log(sport_dist)^2) +
                            Weekend*log(metro_dist) + Weekend*log(tram_dist) + Weekend*log(cs_dist) + Weekend*log(schiphol_dist) +
-                           Holiday*log(metro_dist) + Holiday*log(tram_dist) + Holiday*log(cs_dist) + Holiday*log(schiphol_dist)
-                           , data, na.action = na.omit)
+                           Holiday*log(metro_dist) + Holiday*log(tram_dist) + Holiday*log(cs_dist) + Holiday*log(schiphol_dist) +
+                           neighbourhood_cleansed
+                         , data, na.action = na.omit)
 summary(model_poly_log_int) # Should be including as.factor(neighbourhood_cleansed) but pc couldn't handle it haha
 
+#Save output in csv
+library(broom)
+write.csv(tidy(model_all_int_hol_wkend) , "../../gen/output/model_all_int_hol_wkend.csv")
+write.csv(tidy(model_poly_log_int) , "../../gen/output/model_poly_log_int.csv")
 
