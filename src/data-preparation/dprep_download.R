8720da382a4724a7428e436ca089306ce583ea5d
Kamila Maj <k.majdlenova@tilburguniversity.edu>
2022-03-26 20:33:08 +0100

    add files to download (park, religion, swim, sport)

--- src/data-preparation/dprep_download.R
+++ src/data-preparation/dprep_download.R
@@ -8,7 +8,10 @@ downloads <- list(c(id = "1MLHy1bBmdO16cE51vAAIctJgAxFqAR4L", name = 'holidays_n
                   c(id = "1eVjkmqxJ1q_bHOJ3DPnY0vAvq4nPSEFQ", name = "calendar"),
                   c(id = "1KRSJk45onLk5SHg13w8pS55sIvCvNXPy", name = "listings"),
                   c(id = "1y3IDsiUVEOBynZohi-nD0JUjm9ah16nq", name = "tram_metro_stops"),
-                  c(id = "1isIlj0o7RLsypIspy6BvbREUuPML2ogh", name = "distances_infra"))
+                  c(id = "1swshy6dREsJf7N4jlsMzFXZrB9255k7K", name = "park"),
+                  c(id = "1Km47bM-oQIqBZzl8OmOhrvtdZGxsB6Yz", name = "religion"),
+                  c(id = "19P_lk6b_rY_m2HzZxhg-9wz3ToFgBKd7", name = "sport"),
+                  c(id = "1gVFhGvGe0sNMQtbKzfKtp7HK_MsW_02Z", name = "swimmingpools"))
 
 for (file in downloads) {
   drive_download(as_id(file['id']), 
